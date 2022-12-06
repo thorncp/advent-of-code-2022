46edaaea92dd1d887657b218de4a7229d619cfd4
Chris Thorn <chris@thorn.co>
2022-12-06 10:34:22 -0800

    Slightly better name

--- day6/code.rb
+++ day6/code.rb
@@ -1,4 +1,4 @@
-class DetectMessageStart
+class DetectCommunication
   def self.call(input, marker_size: 4)
     input.chars.each_cons(marker_size).with_index do |chars, index|
       if chars.uniq.size == marker_size
@@ -10,23 +10,23 @@ end
 
 if ARGV.any?
   input = ARGF.read
-  puts "Communication detected at: #{DetectMessageStart.call(input, marker_size: 4)}"
-  puts "Messages starts at: #{DetectMessageStart.call(input, marker_size: 14)}"
+  puts "Communication detected at: #{DetectCommunication.call(input, marker_size: 4)}"
+  puts "Messages starts at: #{DetectCommunication.call(input, marker_size: 14)}"
 else
   require "rspec/autorun"
 
-  RSpec.describe DetectMessageStart do
+  RSpec.describe DetectCommunication do
     it "detects the start of a message" do
-      expect(DetectMessageStart.call("bvwbjplbgvbhsrlpgdmjqwftvncz", marker_size: 4)).to eq 5
-      expect(DetectMessageStart.call("nppdvjthqldpwncqszvftbrmjlhg", marker_size: 4)).to eq 6
-      expect(DetectMessageStart.call("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", marker_size: 4)).to eq 10
-      expect(DetectMessageStart.call("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", marker_size: 4)).to eq 11
+      expect(DetectCommunication.call("bvwbjplbgvbhsrlpgdmjqwftvncz", marker_size: 4)).to eq 5
+      expect(DetectCommunication.call("nppdvjthqldpwncqszvftbrmjlhg", marker_size: 4)).to eq 6
+      expect(DetectCommunication.call("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", marker_size: 4)).to eq 10
+      expect(DetectCommunication.call("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", marker_size: 4)).to eq 11
 
-      expect(DetectMessageStart.call("mjqjpqmgbljsphdztnvjfqwrcgsmlb", marker_size: 14)).to eq 19
-      expect(DetectMessageStart.call("bvwbjplbgvbhsrlpgdmjqwftvncz", marker_size: 14)).to eq 23
-      expect(DetectMessageStart.call("nppdvjthqldpwncqszvftbrmjlhg", marker_size: 14)).to eq 23
-      expect(DetectMessageStart.call("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", marker_size: 14)).to eq 29
-      expect(DetectMessageStart.call("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", marker_size: 14)).to eq 26
+      expect(DetectCommunication.call("mjqjpqmgbljsphdztnvjfqwrcgsmlb", marker_size: 14)).to eq 19
+      expect(DetectCommunication.call("bvwbjplbgvbhsrlpgdmjqwftvncz", marker_size: 14)).to eq 23
+      expect(DetectCommunication.call("nppdvjthqldpwncqszvftbrmjlhg", marker_size: 14)).to eq 23
+      expect(DetectCommunication.call("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", marker_size: 14)).to eq 29
+      expect(DetectCommunication.call("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", marker_size: 14)).to eq 26
     end
   end
 end
