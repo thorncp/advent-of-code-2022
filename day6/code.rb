d4a92eca0ac092f6c0672697959567308fd40124
Chris Thorn <chris@thorn.co>
2022-12-06 10:32:51 -0800

    Day 6 Part 2

--- day6/code.rb
+++ day6/code.rb
@@ -1,26 +1,32 @@
 class DetectMessageStart
-  MARKER_SIZE = 4
-
-  def self.call(input)
-    input.chars.each_cons(4).with_index do |chars, index|
-      if chars.uniq.size == MARKER_SIZE
-        return index + MARKER_SIZE
+  def self.call(input, marker_size: 4)
+    input.chars.each_cons(marker_size).with_index do |chars, index|
+      if chars.uniq.size == marker_size
+        return index + marker_size
       end
     end
   end
 end
 
 if ARGV.any?
-  puts DetectMessageStart.call(ARGF.read)
+  input = ARGF.read
+  puts "Communication detected at: #{DetectMessageStart.call(input, marker_size: 4)}"
+  puts "Messages starts at: #{DetectMessageStart.call(input, marker_size: 14)}"
 else
   require "rspec/autorun"
 
   RSpec.describe DetectMessageStart do
     it "detects the start of a message" do
-      expect(DetectMessageStart.call("bvwbjplbgvbhsrlpgdmjqwftvncz")).to eq 5
-      expect(DetectMessageStart.call("nppdvjthqldpwncqszvftbrmjlhg")).to eq 6
-      expect(DetectMessageStart.call("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).to eq 10
-      expect(DetectMessageStart.call("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).to eq 11
+      expect(DetectMessageStart.call("bvwbjplbgvbhsrlpgdmjqwftvncz", marker_size: 4)).to eq 5
+      expect(DetectMessageStart.call("nppdvjthqldpwncqszvftbrmjlhg", marker_size: 4)).to eq 6
+      expect(DetectMessageStart.call("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", marker_size: 4)).to eq 10
+      expect(DetectMessageStart.call("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", marker_size: 4)).to eq 11
+
+      expect(DetectMessageStart.call("mjqjpqmgbljsphdztnvjfqwrcgsmlb", marker_size: 14)).to eq 19
+      expect(DetectMessageStart.call("bvwbjplbgvbhsrlpgdmjqwftvncz", marker_size: 14)).to eq 23
+      expect(DetectMessageStart.call("nppdvjthqldpwncqszvftbrmjlhg", marker_size: 14)).to eq 23
+      expect(DetectMessageStart.call("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", marker_size: 14)).to eq 29
+      expect(DetectMessageStart.call("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", marker_size: 14)).to eq 26
     end
   end
 end
